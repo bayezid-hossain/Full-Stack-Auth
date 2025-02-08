18f1cd7c11e2fddb3c5847486fb1baeccecb0ad5
Bayezid <arkantos.tas@gmail.com>
2025-02-08 12:29:32 +0600

    sign up frontend done

--- apps/web/app/auth/signup/SignupForm.tsx
+++ apps/web/app/auth/signup/SignupForm.tsx
@@ -1,12 +1,23 @@
+"use client";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import SubmitButton from "@/components/ui/SubmitButton";
-import React from "react";
+import { signup } from "@/lib/auth";
+import React, { useActionState } from "react";
+import { useFormState } from "react-dom";
 
 const SignupForm = () => {
+  const [state, action] = useActionState(signup, undefined);
+
   return (
-    <form>
+    <form action={action}>
       <div className="flex flex-col gap-4">
+        {state?.message && (
+          <p className="text-sm text-red-700">
+            {state.message}
+          </p>
+        )}
+
         <div>
           <Label htmlFor="name">Name</Label>
           <Input
@@ -14,7 +25,13 @@ const SignupForm = () => {
             name="name"
             placeholder="John Doe"
           />
+          {state?.error?.name && (
+            <p className="text-sm text-red-700">
+              {state.error.name}
+            </p>
+          )}
         </div>
+
         <div>
           <Label htmlFor="email">Email</Label>
           <Input
@@ -22,7 +39,13 @@ const SignupForm = () => {
             name="email"
             placeholder="john@example.com"
           />
+          {state?.error?.email && (
+            <p className="text-sm text-red-700">
+              {state.error.email}
+            </p>
+          )}
         </div>
+
         <div>
           <Label htmlFor="password">Password</Label>
           <Input
@@ -30,6 +53,18 @@ const SignupForm = () => {
             name="password"
             type="password"
           />
+          {state?.error?.password && (
+            <div className="text-sm text-red-700">
+              <p>Password must:</p>
+              <ul>
+                {state.error.password.map((error) => (
+                  <li className="text-xs" key={error}>
+                    {error}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
         <SubmitButton>Sign Up</SubmitButton>
       </div>
